c7a85da453396713beaccba72d27f34d82a4e99c
Harish Krishna <harish.krsn@gmail.com>
2014-04-22 14:23:51 -0400

    grouping by place name and adding a fast filter to typeahead sort through the list

--- js/index.js
+++ js/index.js
@@ -4,16 +4,34 @@ var contentsToList = function (contents){
     if (contents.length > 0)
     {
         var ul = $('<ul/>');
+        var lists     = [];
         contents.each(function(){
             var $this = $(this);
-            var key   = $this.children('key').text()
-            var li 	  = $('<li/>');
+            var key   = $this.children('key').text();
+            var name  = key.substring(0,key.indexOf('.'));
+            var format= key.substring(key.indexOf('.')+1);
+
+            var exists= lists.hasOwnProperty(name);
+            var li    = $('<li/>');
+            if (exists) {
+                li = lists[name];
+            } else {
+                lists = [];
+                li.addClass(name);
+                li.append("<span class ='place_name'>" + name + "</span>");
+                lists[name] = li;
+            }
+
             var a     = $('<a/>',{
-                text: key,
+                text: format,
                 href: url + "/" + key
             });
+            a.addClass("format");
+            li.append(" ");
             li.append(a);
-            ul.append(li);
+            if (!exists) {
+                ul.append(li);
+            }
         });
         return ul;
     }
@@ -28,6 +46,7 @@ $(function(){
 		success: function(data) {
 			var contents = $(data).children("ListBucketResult").children("Contents");
 			$("#extracts").html(contentsToList(contents));
+            $('#search_input').fastLiveFilter('#extracts ul');
 		},
 		error: function(request, status, error) {
 			$("#extracts").html(request.responseText)
